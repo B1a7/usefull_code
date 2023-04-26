5aa44839af70d9ed1d0df020618ce829c0dfe60e
Blazej <blazejfurgala@outlook.com>
2023-04-26 10:25:22 +0200

    StringLiteral in c#11

---
+++ CSharp11Features/ListPatterns/Program.cs
@@ -0,0 +1,16 @@
+
+
+int[] numbers = { 1, 2, 3 };
+
+var pattern1 = numbers is [1, 2, 3]; // true
+var pattern2 = numbers is [1, 2]; // false 
+var pattern3 = numbers is [_, 2, 3]; // true [0] can be any 
+var pattern4 = numbers is [1, 2,  > 2 and < 5]; // true 2 < [2] < 5 
+var pattern5 = numbers is [1, 2, 3]; // true
+
+int[] numbers2= { 1, 2, 3, 4, 5 };
+
+var pattern5 = numbers is [1, 2, ..]; // true
+var pattern5 = numbers is [.., 4, 5]; // true
+var pattern5 = numbers is [1, .., 5]; // true
+
---
+++ CSharp11Features/StringLiteral/StringLiteral.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class StringLiteral
+{
+	public StringLiteral()
+	{
+		//old way 
+
+		string filemname1 = "c:\\documents\\file.txt";
+        string filemname2 = @"c:\documents\file.txt";
+        string filemname3 = @"c:\docu""ments\file.txt";
+
+        // C#11 feature string literal
+        string filemname4 = """c:\docu""ments\file.txt""";
+        string filemname4 = $$"""
+            c:\docu""
+            ments\file. is {{someFile}}
+            txt
+            {
+                "name": "Firstname"
+            }
+            """;
+
+        var rating = 3;
+
+        var message = $"rating of {rating switch
+        {
+            < 3 => "sucks",
+            _ => "notbad"
+        }}";
+
+        // UTF8 encoding
+
+        var oldEncoding = Encoding.UTF8.GetBytes("test value");
+
+        var newEncoding = "test value".u8.ToArray(); // only on string hardcoded in our code
+
+    }
+}
